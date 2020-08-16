ff28523f0cc6e85a763fcaa7a27ee633c6524ea7
sai_madhuri <skuntamukkala@deakin.edu.au>
2020-08-16 15:12:26 +1000

    feat: add wallet account number with auto increment, add wallet balance

--- SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/admin/createNewUser.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/admin/createNewUser.js
@@ -4,77 +4,50 @@ const user = require("../../models/user");
 
 exports.CreateUser = (req, res) => {
   const newUser = new NewUser(req.body);
-  
+
   newUser.save((err, newUser) => {
     if (err) {
-      console.log(err);
       res.status(400).json({
         error: "Not able to Create User",
       });
       return;
     }
 
-    userBalance.find((err,val) => {
-      if(err){
-        console.log("Cannot find documents");
-      }
-      else {
-        var len = val.length;
-        
-        if(len==0){
-          var prefix = "SFW00";
-          var  addAccountNumber = 1;
-        }
-        else if(len>0 && len<9){
-        var splitaccountnumber = val[len-1].walletAccountNumber.split("00");
-        var addAccountNumber = parseInt(splitaccountnumber[1])+1;
-        var prefix = "SFW00";
-        } else if(len==9){
-        var splitaccountnumber = val[len-1].walletAccountNumber.split("00");
-        var addAccountNumber = parseInt(splitaccountnumber[1])+1;
-        var prefix = "SFW0";
-        }
-        else if(len>9 && len<99){
-        var splitaccountnumber = val[len-1].walletAccountNumber.split("W0");
-        var addAccountNumber = parseInt(splitaccountnumber[1])+1;
-        var prefix = "SFW0";
-        } 
-        else if(len==99){
-          var splitaccountnumber = val[len-1].walletAccountNumber.split("W0");
-          var addAccountNumber = parseInt(splitaccountnumber[1])+1;
-          var prefix = "SFW";
-        }
-        else {
-        var splitaccountnumber = val[len-1].walletAccountNumber.split("W");
-        var addAccountNumber = parseInt(splitaccountnumber[1])+1;
-        var prefix = "SFW";
-        }
-      var walletaccnum = prefix + addAccountNumber;
-    }
-    //Updating balance collection after admin adds new user
-    var updateUser = {
-      accountNumber: req.body.accountNumber,
-      walletAccountNumber: walletaccnum,
-      accBalance: req.body.openingBalance,
-      walletAccBalance: 0,
-      lowRiskFund: 0,
-      mediumRiskFund: 0,
-      highRiskFund: 0,
-      totalfunds: 0,
-    };
-    
-    const newBalance = new userBalance(updateUser);
-
-    newBalance.save((err, newBalance) => {
-      //console.log(newBalance);
+    userBalance.find((err, docs) => {
       if (err) {
         res.status(400).json({
-          error: "not able to update user information",
+          error: "Not able to find user information",
         });
-        return;
+      } else {
+        var prefix = "SFW";
+        var len = docs.length;
+        var increment = len + 1;
+        var walletaccountnumber =
+          prefix + increment.toString().padStart(3, "0");
       }
+      //Updating balance collection after admin adds new user
+      var updateUser = {
+        accountNumber: req.body.accountNumber,
+        walletAccountNumber: walletaccountnumber,
+        accountBalance: req.body.openingBalance,
+        walletAccountBalance: 0,
+        lowRiskFund: 0,
+        mediumRiskFund: 0,
+        highRiskFund: 0,
+        totalfunds: 0,
+      };
+
+      const newBalance = new userBalance(updateUser);
+
+      newBalance.save((err, newBalance) => {
+        if (err) {
+          res.status(400).json({
+            error: "not able to update user information",
+          });
+          return;
+        }
+      });
     });
-  });
 
     //Updating user table with user credentials
     var userInfo = {
@@ -82,18 +55,18 @@ exports.CreateUser = (req, res) => {
       email: req.body.emailId,
     };
 
-    const createUser = new user(userInfo)
+    const createUser = new user(userInfo);
     createUser.save((err, createUser) => {
       if (err) {
-        // res.status(400).json({
-        //   error: "not able to update user information",
-        // });
+        res.status(400).json({
+          error: "not able to update user information",
+        });
         return;
       }
     });
 
     res.json({ newUser });
-  });  
-  };
+  });
+};
 
-exports.UpdateBalance = (req, res) => {}
+exports.UpdateBalance = (req, res) => {};
--- SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/newTransaction.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/newTransaction.js
@@ -2,64 +2,69 @@ const Transaction = require("../../models/newTransaction");
 const Abalance = require("../../models/balance");
 
 exports.AddTransaction = (req, res) => {
-	const accountNumber = req.body.account;
-	console.log(req.body);
-	//var accNumber = 123;
-	const transaction = new Transaction(req.body);
-	transaction.save((err, transaction) => {
-		if (err) {
-			return res.status(400).json({
-				error: "Not able to save transaction",
-			});
-		}
-		res.json({ transaction });
-	});
-	Abalance.find({ accountNumber }).exec((err, users) => {
-		// console.log(users);
-		if (err || users.length == 0) {
-			return res.status(400).json({
-				error: "Not able to update transaction",
-			});
-		}
-		var accountBalance =
-			parseFloat((parseFloat(users[0].accBalance) -
-			parseFloat(req.body.amount) -
-			parseFloat(req.body.roundedAmount)).toFixed(2));
+  const accountNumber = req.body.account;
+  console.log(req.body);
+  
+  const transaction = new Transaction(req.body);
+  transaction.save((err, transaction) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Not able to save transaction",
+      });
+    }
+    res.json({ transaction });
+  });
+  Abalance.find({ accountNumber }).exec((err, users) => {
+    
+    if (err || users.length == 0) {
+      return res.status(400).json({
+        error: "Not able to update transaction",
+      });
+    }
+    var accountBalance = parseFloat(
+      (
+        parseFloat(users[0].accountBalance) -
+        parseFloat(req.body.amount) -
+        parseFloat(req.body.roundedAmount)
+      ).toFixed(2)
+    );
 
-		const fundType = req.body.fund;
-		const fundBalance =
-			parseFloat(users[0][req.body.fund]) + parseFloat(req.body.roundedAmount);
+    const fundType = req.body.fund;
+    const fundBalance =
+      parseFloat(users[0][req.body.fund]) + parseFloat(req.body.roundedAmount);
 
-		const totalFundsBalance = parseFloat((users[0].totalfunds + parseFloat(req.body.roundedAmount)).toFixed(2))
-		console.log(fundType, fundBalance, totalFundsBalance);
-		updateBalance(
-			accountNumber,
-			accountBalance,
-			{ [fundType]: fundBalance },
-			totalFundsBalance
-		);
+    const totalFundsBalance = parseFloat(
+      (users[0].totalfunds + parseFloat(req.body.roundedAmount)).toFixed(2)
+    );
+    console.log(fundType, fundBalance, totalFundsBalance);
+    updateBalance(
+      accountNumber,
+      accountBalance,
+      { [fundType]: fundBalance },
+      totalFundsBalance
+    );
 
-		if (err || users == null) {
-			return res.status(400).json({
-				error: "Not able to update balance",
-			});
-		}
-	});
+    if (err || users == null) {
+      return res.status(400).json({
+        error: "Not able to update balance",
+      });
+    }
+  });
 };
 
 const updateBalance = (
-	accountNumber,
-	accountBalance,
-	fundOption,
-	totalsavedfunds
+  accountNumber,
+  accountBalance,
+  fundOption,
+  totalsavedfunds
 ) =>
-	Abalance.updateOne(
-		{ accountNumber },
-		{
-			accBalance: accountBalance,
-			...fundOption,
-			totalfunds: totalsavedfunds,
-		}
-	).exec((err, balances) => {
-		console.log(balances);
-	});
+  Abalance.updateOne(
+    { accountNumber },
+    {
+      accountBalance: accountBalance,
+      ...fundOption,
+      totalfunds: totalsavedfunds,
+    }
+  ).exec((err, balances) => {
+    console.log(balances);
+  });
