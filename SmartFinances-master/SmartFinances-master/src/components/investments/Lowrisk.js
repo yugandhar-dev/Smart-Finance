caf330978d1ad2f457b252248c8c9a87c8e4b73e
sprabhala <sprabhala@deakin.edu.au>
2020-08-25 18:34:46 +1000

    feat: add low risk screen in investment tab
    - add description in the tab

---
+++ SmartFinances-master/SmartFinances-master/src/components/investments/Lowrisk.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Addfunds from './Addfunds';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "100vh",
+  },
+
+  paper: {
+    margin: theme.spacing(8, 8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    alignContent: "center",
+    alignSelf: "center",
+    justifyItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1), //tilting the page up and down
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+
+
+
+
+
+function Lowrisk() {
+
+  const [status, currentStatus] = useState(null);
+ 
+
+
+
+  
+  const classes = useStyles();
+  return (
+    <div>
+      {status !== null ? (
+        <div>{status === "otp" ? ""  :""}</div>
+      ) : (
+
+          <Grid
+            container
+            direction="column"
+            justify="center"
+            alignItems="center"
+            component="main"
+            className={classes.root}
+          >
+            <CssBaseline />
+            <Grid
+              item
+              xs={false}
+              sm={10}
+              md={5}
+              component={Paper}
+              elevation={10}
+              square
+            >
+
+              <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                  Low Risk
+            </Typography>
+
+            <p>An Investment where there is perceived to be just a slight chance of loosing some or all of the your money.An Investment where there is perceived to be just a slight chance of loosing some or all of the your money.An Investment where there is perceived to be just a slight chance of loosing some or all of the your money.An Investment where there is perceived to be just a slight chance of loosing some or all of the your money.An Investment where there is perceived to be just a slight chance of loosing some or all of the your money</p>
+
+                <form className={classes.form} id="forms">
+                 
+                  
+                   
+                      <Button
+                        onClick={() => currentStatus("otp")}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                      >
+
+                        Invest
+              </Button>
+                    
+                  
+
+                </form>
+              </div>
+            </Grid>
+          </Grid>
+
+        )}
+
+
+     
+    </div>
+
+  );
+}
+
+
+export default Lowrisk;
