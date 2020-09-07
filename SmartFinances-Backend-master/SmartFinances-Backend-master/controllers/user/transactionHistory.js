c536a79e5707f686dfeac03c66555f9986a428ab
sai_madhuri <skuntamukkala@deakin.edu.au>
2020-09-07 20:24:44 +1000

    feat: initial commit for transactionhistory feature

--- SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/transactionHistory.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/transactionHistory.js
@@ -1,13 +1,21 @@
 const transactions = require("../../models/newTransaction");
 
 exports.GetTransactions = (req, res) => {
+<<<<<<< HEAD
     
     transactions.find({walletAccountNumber: req.body.walletAccountNumber, category: req.body.category}).exec((err,userTransactions) => {
+=======
+    transactions.find({walletAccountNumber: req.body.walletAccountNumber}).exec((err,userTransactions) => {
+>>>>>>> 6f31fc7... feat: initial commit for transactionhistory feature
         if(err){
             return res.status(400).json({
                 error: err
             });
+<<<<<<< HEAD
         }else if(userTransactions.length == 0){
+=======
+        }else if(!userTransactions){
+>>>>>>> 6f31fc7... feat: initial commit for transactionhistory feature
             return res.status(400).json({
                 error: "No User Transactions Found"
             });
