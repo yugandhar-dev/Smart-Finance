bd08990415415d03b4833b972d24a2f7758ee248
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-09-09 13:27:37 +1000

    test(jestandsupertest): add test case for getting  and checking the token

--- SmartFinances-Backend-master/SmartFinances-Backend-master/__tests__/api.test.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/__tests__/api.test.js
@@ -1,6 +1,33 @@
 const request = require("supertest");
 const app = require("../app");
 
+const userCredentials = {
+  email: "usertest@gmail.com",
+  password: "1234",
+  role: "user",
+};
+
+var token, signinResponse;
+
 describe("checks user details", () => {
-    
-})
+  beforeEach(async () => {
+    const response = await request(app)
+      .post("/api/signin")
+      .send(userCredentials);
+    signinResponse = response.body;
+    token = response.body.token;
+  });
+
+  it("checks the token", async (done) => {
+    expect(signinResponse).toMatchObject({
+      token: expect.any(String),
+      user: {
+        email: expect.stringMatching(userCredentials.email),
+        role: expect.stringMatching(userCredentials.role),
+        _id: expect.any(String),
+        name: expect.any(String),
+      },
+    });
+    done();
+  });
+});
