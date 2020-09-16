ca47c4b75e7efc27f19628374b9c4891d7bf05a4
Hassan Abbas Rahupoto <hrahupoto@deakin.edu.au>
2020-09-16 13:32:16 +1000

    test: add test case for checking elements on investment sell screen

--- SmartFinances-master/SmartFinances-master/cypress/integration/examples/InvestmentSell.spec.js
+++ SmartFinances-master/SmartFinances-master/cypress/integration/examples/InvestmentSell.spec.js
@@ -18,13 +18,129 @@ describe("Investment sell screen test", () => {
       .invoke("text")
       .should("eq", "Investments");
 
-      cy.get(".sc-eCApGN.iobEpJ")
+    cy.get(".sc-eCApGN.iobEpJ")
       .children("div")
       .eq(2)
       .should("be.visible")
       .click()
       .invoke("text")
       .should("eq", " Sell Investments");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(0)
+      .should("be.visible")
+      .invoke("text")
+      .should("eq", "Choose the investment type:");
+
+    cy.get("#investments").should("be.visible").click();
+
+    cy.wait(1000);
+
+    cy.get('[aria-labelledby="investment-options"]')
+      .children("li")
+      .eq(0)
+      .should("be.visible")
+      .click()
+      .invoke("text")
+      .should("eq", "lowRiskFund");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(2)
+      .should("be.visible")
+      .invoke("text")
+      .should("eq", "Choose the company:");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(4)
+      .children("div")
+      .eq(0)
+      .should("be.empty");
+
+    cy.get("#companies").should("be.visible").click();
+
+    cy.get('[aria-labelledby="companies"]')
+      .children("li")
+      .eq(0)
+      .should("be.visible")
+      .click()
+      .invoke("text")
+      .should("eq", "ANZ");
+
+    cy.wait(1000);
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(4)
+      .children("div")
+      .eq(0)
+      .should("not.be.empty");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(5)
+      .should("be.visible")
+      .invoke("text")
+      .should("eq", "Enter number of units you want to sell:");
+
+    cy.get("p").eq(2).invoke("text").should("be.empty");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(6)
+      .children("div")
+      .eq(0)
+      .should("be.visible")
+      .type("1");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(6)
+      .children("div")
+      .eq(1)
+      .click();
+
+    cy.get("p").eq(2).invoke("text").should("not.be.empty");
+
+    cy.get(".MuiList-root.MuiList-padding")
+      .children("li")
+      .eq(7)
+      .invoke("text")
+      .should("eq", "Amount:");
+
+    cy.contains("Reset").click();
+
+    cy.get("#investments").should("be.visible").click();
+
+    cy.get('[aria-labelledby="investment-options"]')
+      .children("li")
+      .eq(1)
+      .should("be.visible")
+      .click()
+      .invoke("text")
+      .should("eq", "savingScheme");
+
+    cy.get("#companies").should("be.visible").click();
+
+    cy.get('[aria-labelledby="companies"]')
+      .children("li")
+      .eq(0)
+      .should("be.visible")
+      .click()
+      .invoke("text")
+      .should("eq", "ME Term Deposit");
+
+    cy.get("#companies").should("be.visible").click();
+
+    cy.get('[aria-labelledby="companies"]')
+      .children("li")
+      .eq(1)
+      .should("be.visible")
+      .click()
+      .invoke("text")
+      .should("eq", "AMP Term Deposit");
   });
   
 });
