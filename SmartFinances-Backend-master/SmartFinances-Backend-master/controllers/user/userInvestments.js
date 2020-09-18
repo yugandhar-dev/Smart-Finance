9c151c69b2d8307776c2071ca82383d60c84a55d
bvnpraneeth <vboggarapa@deakin.edu.au>
2020-09-18 11:11:26 +1000

    fix: rebasing to clear conflicts

--- SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/userInvestments.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/user/userInvestments.js
@@ -1,6 +1,7 @@
 const investmentOptions = require("../../models/investmentOptions");
 const userBalance = require("../../models/balance");
 const userInvestments = require("../../models/userInvestments");
+<<<<<<< HEAD
 const newTransaction = require("../../models/newTransaction");
 
 exports.InvestmentFunds = async (req, res) => {
@@ -17,6 +18,18 @@ exports.InvestmentFunds = async (req, res) => {
 
     
 
+=======
+exports.InvestmentFunds = async (req, res) => {
+  const investment = new userInvestments(req.body);
+  //const walletAccountNumber = req.body.walletAccountNumber;
+  //const numberOfUnits = req.body.numberOfUnits;
+  //const savingSchemeAmount = req.body.savingSchemeAmount;
+  let investmentDetails;
+  let userBalances;
+  let calculateAmount;
+  if (investment.investmentType=="lowRiskFund" || investment.investmentType=="exchangeTradedFund" || investment.investmentType=="savingScheme"){
+
+>>>>>>> 78dbb30... fix: rebasing to clear conflicts
   try {
     investmentDetails = await investmentOptions
       .findOne({
@@ -31,6 +44,7 @@ exports.InvestmentFunds = async (req, res) => {
   }
 
   if (!investmentDetails) {
+<<<<<<< HEAD
     return res.status(400).json({
       error: "No Investment options found with the provided company name and investment type"
     });
@@ -166,10 +180,32 @@ exports.InvestmentFunds = async (req, res) => {
     });
   }
 =======
+=======
+    await investment.save();
+    res.json(investment);
+  } else if (investment.investmentType != "savingScheme") {
+    investmentDetails.pricePerUnit = investment.pricePerUnit;
+    await investmentDetails.save();
+  }
+
+  try {
+    userBalances = await userBalance
+      .findOne({ walletAccountNumber: investment.walletAccountNumber })
+      .exec();
+  } catch (err) {
+    
+    return res.status(400).json({
+        
+      error: err
+    });
+  }
+
+>>>>>>> 78dbb30... fix: rebasing to clear conflicts
   if (!userBalances) {
     return res.status(400).json({
       error: "Error fetching user Balances"
     });
+<<<<<<< HEAD
   }else if(userBalances.walletAccountBalance < calculateAmount){
     return res.status(400).json({
       error: "Balance is less than the Investment Amount"
@@ -208,6 +244,92 @@ exports.InvestmentFunds = async (req, res) => {
     res.status(200).json({
       Success: "Amount Invested and updated Balances"
     });
+=======
+  } else if (investment.investmentType != "savingScheme") {
+    calculateAmount =
+      parseFloat(investmentDetails.pricePerUnit) * parseInt(numberOfUnits);
+
+    userBalances.walletAccountBalance =
+      parseFloat(userBalances.walletAccountBalance) - calculateAmount;
+    userBalances[investment.investmentType] =
+      parseFloat(userBalances[investment.investmentType]) +
+      calculateAmount;
+    userBalances.totalfunds = parseFloat(userBalances.totalfunds) + calculateAmount;
+    userBalances.save();
+  } else {
+    userBalances.walletAccountBalance =
+      userBalances.walletAccountBalance - investment.pricePerUnit;
+    userBalances[investment.investmentType] =
+      userBalances[investment.investmentType] + investment.pricePerUnit;
+    userBalances.totalfunds = userBalances.totalfunds + investment.pricePerUnit;
+    await userBalances.save();
+  }
+
+  await userInvestments
+    .findOne({
+      walletAccountNumber: investment.walletAccountNumber,
+      companyName: investment.companyName
+    })
+    .exec((err, investments) => {
+      if (err) {
+        
+        return res.status(400).json({
+            
+          error: err
+        });
+      } else if (!investments && investment.investmentType != "savingScheme") {
+        invest(
+          false,
+          parseInt(numberOfUnits),
+          calculateAmount,
+          investment.pricePerUnit
+        );
+      } else if (!investments && investment.investmentType == "savingScheme") {
+        invest(false, 1, investment.pricePerUnit, investment.pricePerUnit);
+      } else {
+        invest(
+          true,
+          parseInt(investments.numberOfUnits) + parseInt(numberOfUnits),
+          investments.amountInvested + calculateAmount,
+          investment.pricePerUnit
+        );
+      }
+    
+    });
+
+  const invest = async (check, numberOfUnits, amountInvested, pricePerUnit) => {
+    const userinvest = {
+      accountNumber: userBalances.accountNumber,
+      walletAccountNumber: investment.walletAccountNumber,
+      investmentType: investment.investmentType,
+      companyName: investment.companyName,
+      numberOfUnits: parseInt(numberOfUnits),
+      amountInvested: parseFloat(amountInvested),
+      pricePerUnit: investment.pricePerUnit
+    };
+
+    const saveUserInvestment = new userInvestments(userinvest);
+    if (check) {
+      userInvestments
+        .updateOne(
+          {
+            walletAccountNumber: walletAccountNumber,
+            companyName: investment.companyName
+          },
+          {
+            amountInvested: amountInvested,
+            numberOfUnits: numberOfUnits,
+            pricePerUnit: pricePerUnit
+          }
+        )
+        .exec();
+    } else {
+      await saveUserInvestment.save();
+    }
+  };
+
+  res.json(userBalances);
+>>>>>>> 78dbb30... fix: rebasing to clear conflicts
 }
 else{
   return res.status(400).json({
@@ -215,7 +337,11 @@ else{
     error: "Given Investment Type is not in the list"
   });
 }
+<<<<<<< HEAD
 >>>>>>> 8c0b353... fix: modified and fixed code according to comments
 };
 
   
+=======
+};
+>>>>>>> 78dbb30... fix: rebasing to clear conflicts
