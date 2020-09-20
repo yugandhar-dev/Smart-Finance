912b485dbdf6824ae68cd9ed9f8b25990fe4b852
MonicaRayapati <mrayapati@deakin.edu.au>
2020-09-20 20:32:42 +1000

    feat(settings): add icons to screen

--- SmartFinances-master/SmartFinances-master/src/components/Settings/settings.js
+++ SmartFinances-master/SmartFinances-master/src/components/Settings/settings.js
@@ -9,25 +9,31 @@ import {
 	MenuItem,
 	Select,
 } from "@material-ui/core";
+import {Pencil} from "@styled-icons/boxicons-regular/Pencil"
 export default()=>{
     return(
         <Grid container justify="center">
             <List>
                 <ListItem>
-                    <a>UserName:</a>
-                    <TextField>usertest@gmail.com</TextField>
+                    <td><a>UserName:</a></td>
+                    <td><TextField>usertest@gmail.com</TextField></td>
                 </ListItem>
                 <ListItem>
                     <a>Mobile Number:</a>
                     <TextField>Enter Mobile Number</TextField>
+                    <Pencil size="25" />
                 </ListItem>
                 <ListItem>
                     <a>Password:</a>
                     <TextField>Password</TextField>
+                    <Pencil size="25" />
                 </ListItem>
                 <ListItem>
                     <a>Email:</a>
                     <Button>Contact Us to Change Email Address</Button>
+                </ListItem>
+                <ListItem>
+                    <Button variant="contained" color="primary">Update</Button>
                 </ListItem>
             </List>
         </Grid>
