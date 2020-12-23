9ba743f27f125730ccab508471919dee15cd91f2
Siva Suraj <sgandi@deakin.edu.au>
2020-12-23 21:39:56 +1100

    fix(usersignupdbmodel): the following changes has been made -added route and controller for usersignup page -added the fields that will be appended from the front-end during user signup

--- SmartFinances-Backend-master/SmartFinances-Backend-master/models/usersignup.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/models/usersignup.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 
 const { Schema } = mongoose;
 
-const usersignup = new Schema(
+const Usersignup = new Schema(
   {
     FirstName: {
       type: String,
@@ -17,18 +17,18 @@ const usersignup = new Schema(
       maxlength: 32,
       trim: true
     },
-    DOB: {
-        type: Date,
-        required: false,
-        maxlength: 32,
-        trim: true
-      },
     email: {
       type: String,
       trim: true,
       required: true,
       unique: true
     },
+    DOB: {
+        type: Date,
+        required: false,
+        maxlength: 32,
+        trim: true
+      },
     PhoneNumber: {
       type: Number,
       required: true,
@@ -39,7 +39,7 @@ const usersignup = new Schema(
       type: String,
       required: true
     },
-    StreeetAdrress: {
+    StreetAdrress: {
       type: String,
       required: true,
       maxlength: 64,
@@ -61,11 +61,72 @@ const usersignup = new Schema(
         type: Number,
         required: true,
         trim: true
-    }
+    },
+    isEnrolled: {
+      type: String,
+      required: true,
+    },
+    University: {
+      type: String,
+      required: true,
+      maxlength: 64,
+      trim: true
+    },
+    commuteToUniversity: {
+      type: String,
+      required: true,
+    },
+    isWorking: {
+      type: String,
+      required: true,
+    },
+    Industry: {
+      type: String,
+      required: true,
+      maxlength: 64,
+      trim: true
+    },
+    officeLocation: {
+      type: String,
+      required: true,
+      maxlength: 64,
+      trim: true
+    },
+    commuteToOffice: {
+      type: String,
+      required: true,
+    },
+    bankName : {
+      type : String,
+      trim : true,
+  },
+   accountNumber : {
+      type : Number,
+      required : true,
+      trim : true,
+      unique : true,
+  },
+   tfnNumber : {
+      type : Number,
+      required : true,
+      trim : true,
+  },
+   openingBalance : {
+      type : Number,
+},
+  walletAccountNumber: {
+     type : String,
+     required : true,
+     trim : true,
+     unique : true,
+   },
+   isverified: {
+      type: String,
+      required: true,
+  },
+
   },
   { timestamps: true }
 );
 
-module.exports = {
-  User: mongoose.model("Usersignup", usersignup),
-};
+module.exports = mongoose.model("Usersignup", Usersignup);
