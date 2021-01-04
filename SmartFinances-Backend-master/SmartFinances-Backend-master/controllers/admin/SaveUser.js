1709c9718abf44d6c3ce2457600cac8d353861b1
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2021-01-04 14:31:51 +1100

    feat(save-user): isVerified changes to true

--- SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/admin/SaveUser.js
+++ SmartFinances-Backend-master/SmartFinances-Backend-master/controllers/admin/SaveUser.js
@@ -1,3 +1,15 @@
 const userSignUp = require('../../models/usersignup');
+const adminNewUsers = require('../../models/createNewUser');
+const userLogins = require('../../models/user');
+const balances = require('../../models/balance');
 
-console.log(userSignUp);
+exports.SaveUser = async (req, res) => {
+  try {
+    const user = await userSignUp.findOne({ email: req.body.emailId });
+    user.isVerified = true;
+
+    await user.save();
+  } catch (error) {
+    return res.status(400).json({ error: error });
+  }
+};
