dcfc0eb5839e1ebec28ace626c68530b96d57c26
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2021-01-25 23:50:16 +1100

    added button to transactions history page

--- SmartFinances-master/SmartFinances-master/src/components/Transactions/history.js
+++ SmartFinances-master/SmartFinances-master/src/components/Transactions/history.js
@@ -1,40 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { getTransactionHistory, getUserDetails } from "../../auth/index";
-import Datatable from "./datatable";
+import React, { useState, useEffect } from 'react';
+import { getTransactionHistory, getUserDetails } from '../../auth/index';
+import Datatable from './datatable';
+import Button from '@material-ui/core/Button';
 
 export default () => {
-	const [rows, setRows] = useState([]);
-	const [toggle, setToggle] = useState(false);
-	const [zeroRecords, setZeroRecords] = useState(false);
-	useEffect(() => {
-		const fetchData = async () => {
-			const userDetails = await getUserDetails();
-			try {
-				const history = await getTransactionHistory({
-					walletAccountNumber: userDetails[0].walletAccountNumber,
-				});
-				if (!history["error"]) {
-					setRows(history);
-					setToggle(!toggle);
-				} else {
-					if (history["error"] == "No User Transactions Found")
-						setZeroRecords(true);
-					setToggle(!toggle);
-				}
-			} catch (ex) {
-				console.log(ex);
-			}
-		};
-		fetchData();
-	}, []);
+  const [rows, setRows] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const [zeroRecords, setZeroRecords] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      const userDetails = await getUserDetails();
+      try {
+        const history = await getTransactionHistory({
+          walletAccountNumber: userDetails[0].walletAccountNumber,
+        });
+        if (!history['error']) {
+          setRows(history);
+          setToggle(!toggle);
+        } else {
+          if (history['error'] == 'No User Transactions Found')
+            setZeroRecords(true);
+          setToggle(!toggle);
+        }
+      } catch (ex) {
+        console.log(ex);
+      }
+    };
+    fetchData();
+  }, []);
 
-	return (
-		<div>
-			{rows && rows != [] ? (
-				<Datatable history={rows} toggle={toggle} zeroRecords={zeroRecords} />
-			) : (
-				""
-			)}
-		</div>
-	);
+  return (
+    <div>
+      <div className="print-btn">
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => window.print()}
+          style={{ float: 'right', marginTop: 10 }}
+        >
+          Print Statement
+        </Button>
+      </div>
+      {rows && rows != [] ? (
+        <Datatable history={rows} toggle={toggle} zeroRecords={zeroRecords} />
+      ) : (
+        ''
+      )}
+    </div>
+  );
 };
