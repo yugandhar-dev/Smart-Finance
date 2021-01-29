1f222e72352efa0fbc45daed929d5f316c6f4374
Siva Suraj <sivasuraj486@gmail.com>
2021-01-30 00:41:53 +1100

    test(navigation): added test cases to - verify the user navigation for Questionnaire tab

---
+++ SmartFinances-master/SmartFinances-master/cypress/integration/examples/navigationtest.spec.js
@@ -0,0 +1,18 @@
+describe("navigation test cases", () => {
+
+    it('should check whether visit user login page', () => {
+      cy.visit("http://localhost:3000/user/");
+    });
+    it("should check Navigation test cases", () => {
+      cy.get('#email').type('usertest@gmail.com');
+      cy.get('#password').type('1234');
+      cy.get('#phone').type('+61481831824');
+      //cy.contains("Send OTP").click();
+      cy.wait(3000);
+      //cy.get('#otp').type('456321');
+      cy.get('#sign-in-button').click();
+      cy.wait(5000);
+      cy.contains("Questionnaire").should("exist");
+      cy.contains("Questionnaire").click();
+    });
+})
