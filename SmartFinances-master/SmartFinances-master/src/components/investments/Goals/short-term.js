d9b3a88e74b85efd810754803927bf83f3c832c6
Adassuriyage Don Leelaka Indira <liad@deakin.edu.au>
2021-01-29 15:34:43 +1100

    feat: styling complete

--- SmartFinances-master/SmartFinances-master/src/components/investments/Goals/short-term.js
+++ SmartFinances-master/SmartFinances-master/src/components/investments/Goals/short-term.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormHelperText from '@material-ui/core/FormHelperText';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
-import NativeSelect from '@material-ui/core/NativeSelect';
-import TextField from '@material-ui/core/TextField';
-import Grid from '@material-ui/core/Grid';
+import {List, TextField, Grid, Button, InputLabel, FormHelperText, FormControl, Select, NativeSelect, Paper, CssBaseline} from '@material-ui/core';
 import { Container } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
@@ -16,6 +10,20 @@ const useStyles = makeStyles((theme) => ({
   },
   selectEmpty: {
     marginTop: theme.spacing(2),
+  },
+  container: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(12, 1fr)',
+    gridGap: theme.spacing(3),
+  },
+  Paper: {
+    margin: theme.spacing(8, 8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'center',
+    alignSelf: 'center',
+    justifyItems: 'center',
   },
 }));
 
@@ -35,10 +43,36 @@ export default function UserGoals() {
   };
 
   return (
-    <Container maxWidth="sm">
-    <div>
+    <Grid
+    container
+    direction="column"
+    justify="center"
+    alignItems="center"
+    component="main"
+    className={classes.root}
+  >
+    <CssBaseline />
+    <Grid
+      item
+      xs={false}
+      sm={10}
+      md={10}
+      component={Paper}
+      elevation={10}
+      square
+    >
+      <div className={classes.paper}>
+      <div>
+      <Container maxWidth="xs" justify="center">
+      
       <FormControl className={classes.formControl}>
-        <InputLabel htmlFor='goals-term'>Goal Setting</InputLabel>
+      
+      <Grid container spacing={3}>
+        <Grid item md={6}>
+          <p>Budget your spendings</p>
+        </Grid>
+
+        <Grid item md={4}>
         <NativeSelect
           value={state.goal}
           onChange={handleChange}
@@ -47,30 +81,55 @@ export default function UserGoals() {
             id: 'goals-term',
           }}
         >
-          {/* <option aria-label="Short Term" value="" /> */}
           <option value={10}>Short Term</option>
           <option value={20}>Long Term</option>
         </NativeSelect>
-        {/* <FormHelperText>Select your duration</FormHelperText> */}
-        <Grid container spacing={3}>
-        <Grid item xs={6}>
-          <p>Budget your spendings</p>
         </Grid>
-        <Grid item xs={6}>
+        <Grid item md={6}>
+            <p>
+                Enter Income
+            </p>
+        </Grid>
+        <Grid item md={4}>
         <TextField
           id="standard-number"
-          label="Income"
           type="number"
           InputLabelProps={{
             shrink: true,
           }}
         />
         </Grid>
+        <Grid item md={6}>
+        <NativeSelect
+          value={state.goal}
+          onChange={handleChange}
+          inputProps={{
+            name: 'budget',
+            id: 'budget-list',
+          }}
+        >
+          <option value={10}>Shopping</option>
+          <option value={20}>Gas</option>
+          <option value={30}>Groceries</option>
+          <option value={40}>Eating Out</option>
+          <option value={50}>Rent</option>
+          <option value={60}>Entertainment</option>
+          <option value={70}>Maintenance</option>
+          <option value={80}>Utilities</option>
+          <option value={90}>Entertainment</option>
+          <option value={100}>Other</option>
+        </NativeSelect>
         </Grid>
-
+        <Grid item md={6}>
+          <Button variant="contained" color="primary"> Add new budget </Button>
+        </Grid>
+        </Grid>
       </FormControl>
+      </Container>
     </div>
-    </Container>
-  );
+      </div>
+    </Grid>
+  </Grid>
+);
+};
 
-}
