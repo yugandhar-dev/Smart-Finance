d72e9e8d16b0abf01b075e4a42fab00d7e3a4c8b
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2021-02-02 12:44:32 +1100

    successfully retrieved previous answers and able to modify them

--- SmartFinances-master/SmartFinances-master/src/components/Questionnaire/questionnaire.js
+++ SmartFinances-master/SmartFinances-master/src/components/Questionnaire/questionnaire.js
@@ -5,19 +5,37 @@ import { getAnswers } from '../../auth/index';
 
 export default function Questionnaire() {
   const [formData, setFormData] = useState({});
+  const [message, setMessage] = useState();
 
   useEffect(() => {
     const getprevious = async () => {
       const res = await getAnswers();
-      console.log(res);
+      setFormData(res[0]);
     };
     getprevious();
   }, []);
 
   const submitAns = async e => {
     e.preventDefault();
+    if (
+      formData.Answer1 === '' ||
+      formData.Answer2 === '' ||
+      formData.Answer3 === '' ||
+      formData.Answer4 === '' ||
+      formData.Answer5 === '' ||
+      formData.Answer6 === '' ||
+      formData.Answer7 === '' ||
+      formData.Answer8 === '' ||
+      formData.Answer9 === '' ||
+      formData.Answer10 === ''
+    ) {
+      setMessage('Please answer all the questions');
+      return;
+    }
+
     const res = await submitQuestionnaire(formData);
-    console.log(res);
+    if (res.questions._id)
+      setMessage('Questionnaire has been submitted successfully!');
   };
   return (
     <div>
@@ -36,11 +54,13 @@ export default function Questionnaire() {
                   type="text"
                   name="first_question"
                   id="first_question"
+                  value={formData?.Answer1}
                   onChange={e =>
                     setFormData({ ...formData, Answer1: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
@@ -57,8 +77,10 @@ export default function Questionnaire() {
                   onChange={e =>
                     setFormData({ ...formData, Answer2: e.target.value })
                   }
+                  value={formData?.Answer2}
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
@@ -73,11 +95,13 @@ export default function Questionnaire() {
                   type="text"
                   name="third_question"
                   id="third_question"
+                  value={formData?.Answer3}
                   onChange={e =>
                     setFormData({ ...formData, Answer3: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
@@ -91,11 +115,13 @@ export default function Questionnaire() {
                   type="text"
                   name="fourth_question"
                   id="fourth_question"
+                  value={formData?.Answer4}
                   onChange={e =>
                     setFormData({ ...formData, Answer4: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
@@ -109,107 +135,123 @@ export default function Questionnaire() {
                   type="text"
                   name="fifth_question"
                   id="fifth_question"
+                  value={formData?.Answer5}
                   onChange={e =>
                     setFormData({ ...formData, Answer5: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-group">
               <div className="form-row">
-                <label for="fifth_question">
+                <label for="sixth_question">
                   Have you got any kind of loans or financial commitments with
                   you currently?
                 </label>
                 <input
                   type="text"
-                  name="fifth_question"
-                  id="fifth_question"
+                  name="sixth_question"
+                  id="sixth_question"
+                  value={formData?.Answer6}
                   onChange={e =>
                     setFormData({ ...formData, Answer6: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-group">
               <div className="form-row">
-                <label for="fifth_question">
+                <label for="seventh_question">
                   Have you ever worked with a financial advisor before?
                 </label>
                 <input
                   type="text"
-                  name="fifth_question"
-                  id="fifth_question"
+                  name="seventh_question"
+                  id="seventh_question"
+                  value={formData?.Answer7}
                   onChange={e =>
                     setFormData({ ...formData, Answer7: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-group">
               <div className="form-row">
-                <label for="fifth_question">
+                <label for="eighth_question">
                   Do you feel as if you're currently reaching your goals?{' '}
                 </label>
                 <input
                   type="text"
-                  name="fifth_question"
-                  id="fifth_question"
+                  name="eighth_question"
+                  id="eighth_question"
+                  value={formData?.Answer8}
                   onChange={e =>
                     setFormData({ ...formData, Answer8: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-group">
               <div className="form-row">
-                <label for="fifth_question">
+                <label for="nineth_question">
                   How do you picture your life 1/3/5 years from now?
                 </label>
                 <input
                   type="text"
-                  name="fifth_question"
-                  id="fifth_question"
+                  name="nineth_question"
+                  id="nineth_question"
+                  value={formData?.Answer9}
                   onChange={e =>
                     setFormData({ ...formData, Answer9: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-group">
               <div className="form-row">
-                <label for="fifth_question">
+                <label for="tenth_question">
                   Do you have a household budget?
                 </label>
                 <input
                   type="text"
-                  name="fifth_question"
-                  id="fifth_question"
+                  name="tenth_question"
+                  id="tenth_question"
+                  value={formData?.Answer10}
                   onChange={e =>
                     setFormData({ ...formData, Answer10: e.target.value })
                   }
                   placeholder="Your answer"
                   className="input-text"
+                  required
                 />
               </div>
             </div>
             <div className="form-row-last">
               <input
+                type="button"
                 name="Submit"
                 onClick={e => submitAns(e)}
                 className="register"
                 value="Submit"
               />
+            </div>
+            <div>
+              <p>{message}</p>
             </div>
           </form>
         </div>
