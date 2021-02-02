5af036d8a30040ee1cf0912aa6deb5b7619183f3
Yugandhar Pitchika <ypitchika@deakin.edu.au>
2021-02-02 16:41:20 +1100

    fixed buttons on investment trends page

--- SmartFinances-master/SmartFinances-master/src/components/investments/Lowriskinvest.js
+++ SmartFinances-master/SmartFinances-master/src/components/investments/Lowriskinvest.js
@@ -1,33 +1,33 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Paper from "@material-ui/core/Paper";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Lowtrend from "./Lowtrend";
-import ETFT from "./ETFT";
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Lowtrend from './Lowtrend';
+import ETFT from './ETFT';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxHeight: "100vh",
-    marginTop: "5%",
+    maxHeight: '100vh',
+    marginTop: '5%',
   },
   paper: {
     margin: theme.spacing(8, 8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    alignContent: "center",
-    alignSelf: "center",
-    justifyItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignContent: 'center',
+    alignSelf: 'center',
+    justifyItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1), //tilting the page up and down
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -44,7 +44,13 @@ const Lowrisk = () => {
   return (
     <div>
       {status !== null ? (
-        <div>{status === "invest" ? <Lowtrend setStatus={setStatus} classes={classes}/> : <ETFT setStatus={setStatus} classes={classes}/>}</div>
+        <div>
+          {status === 'invest' ? (
+            <Lowtrend setStatus={setStatus} classes={classes} />
+          ) : (
+            <ETFT setStatus={setStatus} classes={classes} />
+          )}
+        </div>
       ) : (
         <Grid
           container
@@ -69,32 +75,30 @@ const Lowrisk = () => {
                 Low Risk
               </Typography> */}
 
-             
-
               <form className={classes.form} id="forms">
                 <Button
-                  onClick={() => setStatus("invest")}
+                  onClick={() => setStatus('invest')}
                   type="submit"
                   fullWidth
                   variant="contained"
-                  id = "lowrisk"
+                  id="lowrisk"
                   color="primary"
                   className={classes.submit}
                 >
-                  Low risk Trend Analysis
+                  Investments Analysis Trend
                 </Button>
                 <Button
-                  onClick={() => setStatus("etft")}
+                  onClick={() => setStatus('etft')}
                   type="submit"
                   fullWidth
-                  id = "etf"
+                  id="etf"
                   variant="contained"
                   color="primary"
                   className={classes.submit}
                 >
-                  Exchange Traded Funds Trend
+                  Transactions Analysis Trend
                 </Button>
-                <Button
+                {/* <Button
                   onClick={() => setStatus("invest")}
                   type="submit"
                   fullWidth
@@ -104,7 +108,7 @@ const Lowrisk = () => {
                   className={classes.submit}
                 >
                   Savings Scheme
-                </Button>
+                </Button> */}
               </form>
             </div>
           </Grid>
